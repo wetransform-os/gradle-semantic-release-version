64f28989c2b4de8994a184ecafbac5a0710d7978
Simon Templer <simon@wetransform.to>
2025-09-16 08:59:40 +0200

    style: apply spotless formatting

--- src/test/java/to/wetransform/gradle/version/ConfigurableVersionPluginTest.java
+++ src/test/java/to/wetransform/gradle/version/ConfigurableVersionPluginTest.java
@@ -1,10 +1,25 @@
+/*
+ * Copyright 2024 wetransform GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package to.wetransform.gradle.version;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.gradle.api.Project;
 import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConfigurableVersionPluginTest {
   @Test
@@ -21,6 +36,6 @@ public class ConfigurableVersionPluginTest {
     // Verify that version is not set
     assertThat(project.getVersion()).isEqualTo(Project.DEFAULT_VERSION);
 
-    //TODO test default version after evaluation
+    // TODO test default version after evaluation
   }
 }
